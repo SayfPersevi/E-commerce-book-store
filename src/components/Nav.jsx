1c62d03312a10461a54955f8701bff9a7c31ef81
Sayf <Sayf.Persevi@outlook.com>
2023-05-20 18:30:54 +0200

    added menubackdrop

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,6 +3,16 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import LibraryLogo from "../assets/Library.svg";
 
 function Nav() {
+
+    function openMenu() {
+        document.body.classList.add("menu--open");
+    }
+
+    function closeMenu() {
+        document.body.classList.remove("menu--open");
+    }
+
+
     return (
         <nav>
             <div className="nav__container">
@@ -20,7 +30,7 @@ function Nav() {
                          Books  
                         </a>
                     </li>
-                    <button className="btn__menu">
+                    <button className="btn__menu" onClick={openMenu}>
                         <FontAwesomeIcon icon="bars" />
                     </button>
                     <li className="nav__icon">
@@ -31,10 +41,10 @@ function Nav() {
                     </li>
                 </ul>
                 <div className="menu__backdrop">
-                    <button className="btn__menu btn__menu--close">
+                    <button className="btn__menu btn__menu--close" onClick={closeMenu}>
                         <FontAwesomeIcon icon="times" />
                     </button>
-                    <ul>
+                    <ul className="menu__links">
                         <li className="menu__list">
                             <a href="/" className="menu__link">
                                 Home
