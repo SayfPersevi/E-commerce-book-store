afde6a0930dd362fe7c3151b563c336c790a8f97
Sayf <Sayf.Persevi@outlook.com>
2023-05-20 17:22:39 +0200

    added features books

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Highlight from "./Highlight";
+import Highlight from "./ui/Highlight";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Highlights() {
@@ -11,9 +11,27 @@ function Highlights() {
             Why choose <span className="purple">Library</span>
           </h2>
           <div className="highlight__wrapper">
-            <Highlight icon={<FontAwesomeIcon icon="bolt" />} subTitle={"Easy and Quick"} description={"Get acces to the book you purchased online instantly."} />
-            <Highlight icon={<FontAwesomeIcon icon="book-open" />} subTitle={"10.000+ Books"} description={"Library has books in all your favourite categories."} />
-            <Highlight icon={<FontAwesomeIcon icon="tags" />} subTitle={"Affordable"} description={"Get your hands on popular books for as little as $10."} />
+            <Highlight
+              icon={<FontAwesomeIcon icon="bolt" />}
+              subTitle={"Easy and Quick"}
+              description={
+                "Get acces to the book you purchased online instantly."
+              }
+            />
+            <Highlight
+              icon={<FontAwesomeIcon icon="book-open" />}
+              subTitle={"10.000+ Books"}
+              description={
+                "Library has books in all your favourite categories."
+              }
+            />
+            <Highlight
+              icon={<FontAwesomeIcon icon="tags" />}
+              subTitle={"Affordable"}
+              description={
+                "Get your hands on popular books for as little as $10."
+              }
+            />
           </div>
         </div>
       </div>
