4333cec39f36a0dd071d8d5286b1f240b84719ed
Sayf <Sayf.Persevi@outlook.com>
2023-05-22 14:03:20 +0200

    test commit (Nothing changed)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import Nav from "./components/Nav"
 import Footer from "./components/Footer";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route} from "react-router-dom";
 import Home from "./Pages/Home";
 
 function App() {
   return (
     <Router>
+
     <div className="App">
       <Nav />
       <Route path="/" exact component={Home} />
       <Footer />
     </div>
+
     </Router>
   );
 }
