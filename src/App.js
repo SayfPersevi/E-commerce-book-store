7ee04eeaba62a9b5d01e3ca4ee34fa7947105119
Sayf <Sayf.Persevi@outlook.com>
2023-05-23 11:51:55 +0200

    All Books Page Set With Filter

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Nav from "./components/Nav"
 import Footer from "./components/Footer";
 import { BrowserRouter as Router, Route, Link} from "react-router-dom";
 import Home from "./Pages/Home";
+import { books } from "./data";
+import Books from "./Pages/Books";
 
 function App() {
   return (
@@ -10,6 +12,7 @@ function App() {
     <div className="App">
       <Nav />
       <Route path="/" exact component={Home} />
+      <Route path="/books" render={() => <Books books={books} />} />
       <Footer />
     </div>
 
